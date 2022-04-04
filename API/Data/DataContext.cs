6e00ad1b35b9df72c6074232106f5f91f0370b0a
sd2122 <sd2122@bath.ac.uk>
2022-04-04 20:08:44 +0100

    Inital create

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,26 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+
+            optionsBuilder.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+
+    }
+}
